c9e1a07a9b47c9f2a399a6b060fd44a9e0a011d7
Saranga-B <bandarasaranga@icloud.com>
2023-04-10 23:50:45 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import LoginForm from './components/LoginForm/LoginForm';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <LoginForm/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./Footer.css"
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className='sb_footer section_padding'>
+            <div className='sb_footer-links'>
+                 
+            
+           
+            
+
+            </div>
+            <hr></hr>
+            <div className='sb_footer-below'>
+                <div className='description'>
+                    <p>
+                    The Faculty of Engineering of University of Ruhuna
+                     was established on 1st July 1999 at Hapugala, Galle.  Admission to the Faculty of Engineering, University of Ruhuna, is subject to the University Grants Commission policy on university admissions.
+                    </p>
+                </div>
+                
+                <div className='sb_footer-below-links'>
+                    <h1>DEPARTMENTS</h1>
+                    <a href="/terms"><div><p>Electrical and Information Engineering</p></div></a>
+                    <a href="/Privacy"><div><p>Civil and Environmental Engineering</p></div></a>
+                    <a href="/Secutiy"><div><p>Mechanical and Manufacturing Engineering</p></div></a>
+                    <a href="/Cookie"><div><p>Interdisciplinary Studies</p></div></a>
+                </div>
+                <div className='useful-links'>
+                    <h1>USEFUL LINKS</h1>
+                    <a href="/terms"><div><p>University of Ruhuna</p></div></a>
+                    <a href="/Privacy"><div><p>Faculty of Engineering</p></div></a>
+                    <a href="/Secutiy"><div><p>Library</p></div></a>
+                    <a href="/Cookie"><div><p>IESL</p></div></a>
+                </div>
+                <div className='contact-us'>
+                    <h1>CONTACT US</h1>
+                    <a href="/terms"><div><p>Faculty of Engineering,Hapugala,Galle,Sri Lanka.</p></div></a>
+                    <a href="/Privacy"><div><p>Phone: +(94)0 91 2245765/6</p></div></a>
+                    <a href="/Secutiy"><div><p>E-mail: webmaster@eng.ruh.ac.lk</p></div></a>
+                    
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+    
+  )
+}
